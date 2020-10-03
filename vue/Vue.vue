ce153e1a42ea6c707f238e1edd89cd2a6a485f18
Zalimkhan <52750268+zzolik@users.noreply.github.com>
2020-10-03 08:17:02 +0300

    Add files via upload

---
+++ vue/Vue.vue
@@ -0,0 +1,31 @@
+new Vue({
+  el: '#App',
+  data: {
+    sortKey: 'name',
+    reverse: false,
+    search: '',
+    Products: [
+      { name: 'Двери' },
+      { name: 'Пиломатериалы' },
+      { name: 'Лестницы' },
+      { name: 'Окна, стекло' },
+      { name: 'Потолки' },
+      { name: 'Плитка и камни' },
+      { name: 'Мебель' },
+    ],
+  },
+  computed: {
+    sortedUsers() {
+      const k = this.sortKey;
+      return this.Products.sort((a, b) => {
+        return (a[k] < b[k] ? -1 : a[k] > b[k] ? 1 : 0) * [ 1, -1 ][+this.reverse];
+      });
+    },
+    filteredUsers() {
+      const s = this.search.toLowerCase();
+      return this.sortedUsers.filter(n => {
+        return Object.values(n).some(m => m.toString().toLowerCase().includes(s));
+      });
+    },
+  }
+});
